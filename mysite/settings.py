fdd0e3b5f9e1b777f20dd5ad68f40ffbaa288394
Artem <34547317+Zenslash@users.noreply.github.com>
2022-05-30 18:19:37 +0300

    Update settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -135,6 +135,8 @@ STATICFILES_DIRS = [
 os.path.join(BASE_DIR, "static"),
 ]
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATIC_URL = '/static/'
 ALLOWED_HOSTS = ['*']
 X_FRAME_OPTIONS = '*'
