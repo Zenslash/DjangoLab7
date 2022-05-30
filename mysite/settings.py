c404e9859e6bcbed77a3366e5d6bf802b7805680
Artem <34547317+Zenslash@users.noreply.github.com>
2022-05-30 18:58:57 +0300

    Update settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -134,11 +134,11 @@ LANGUAGES = [
 
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-os.path.join(BASE_DIR, "static"),
+
 ]
 
 ALLOWED_HOSTS = ['*']
